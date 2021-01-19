4de331afd84ddaf970f227312d8da053a4c86c35
Sworup Bhattarai <sworup.bhattarai@mavs.uta.edu>
2021-01-19 09:39:02 -0800

    testing testing

--- Testing Testing 1 2 /Test.cpp
+++ Testing Testing 1 2 /Test.cpp
@@ -7,9 +7,31 @@ int main(){
 	string s;
 	s = "#";
 	
-	for (int i = 0; i <= 100 ; i ++){
+	//for loop 
+	for (int i = 1; i <= 6 ; i ++){
 	
-		cout << s << endl;
+		cout << i << " " << s << endl;
 		s += "#";
 	}
+	int count = 1;
+	
+	//while loop
+	while (count <= 12){
+		cout << "Count still not 12 yet. It is currently: " << count << endl;
+		count++;
+	}
+	
+	//do while loop
+	do{
+		if (count != 0){
+			cout << "Count still not 0 yet. It is currently: " << count << endl;
+			count--;
+		} else {
+			cout << "Count is now: " << count << endl;
+			count--;
+		}
+		
+	}while(count >= 0); 
+	
+	
 }
