f6210fff50866330404055418c17f6ac6c6453c0
Sworup Bhattarai <sworup.bhattarai@mavs.uta.edu>
2021-01-19 22:27:55 -0600

    backup and running 01-19 22:27

---
+++ P01/bonus/hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	string name;
+	cout << "What be your name adventurer?" << endl;
+	getline (cin, name);
+	cout << "Hello, " << name << "!" << endl;
+}
+
+
+
---
+++ P01/full_credit/hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello, Sworup!" << endl;
+	return 0;
+}
+
+
+
