b1516fba1549c00eea8c9cd5e5584003b6c93bd9
Sworup Bhattarai <sworup.bhattarai@mavs.uta.edu>
2021-01-20 18:10:23 -0600

    01-20 18:10

--- Testing Testing 1 2 /Test/Test.cpp
+++ Testing Testing 1 2 /Test/Test.cpp
@@ -1,5 +1,6 @@
-
 #include <iostream>
+
+
 using namespace std;
 
 int main(){
@@ -32,6 +33,87 @@ int main(){
 		}
 		
 	}while(count >= 0); 
+	
+	
+	double total = 0;
+	int quarters = 0;
+	int dimes = 0;
+	int nickles = 0;
+	int pennies = 0;
+	cin.clear();
+	cout << "Please enter the number of quarters you have: " ;
+	cin >> quarters;
+	
+	cin.clear();
+	cout << "Please enter the number of dimes you have: " ;
+	cin >> dimes;
+
+	cin.clear();
+	cout << "Please enter the number of nickles you have: " ;
+	cin >> nickles;
+
+	cin.clear();
+	cout << "Please enter the number of pennies you have: " ;
+	cin >> pennies;
+	
+	/*while (true) {
+		
+		try{
+			cin >> quarters;
+			if (cin.fail())
+			{
+				throw 1;
+			}
+			else
+			{
+				break;
+			}
+		}
+		catch (int err){
+			cout << "wrong try again" << endl;
+			cin.clear();
+			cout << "Please enter the number of quarters you have: " << endl;
+			cin >> quarters;
+			cout << quarters;
+		}						
+	}*/
+	
+	cout << quarters << " " <<  dimes << " " << nickles << " " <<pennies << endl;
+	
+	total = quarters * .25;
+	total += (dimes * .10);
+	total += nickles * .05;
+	total += pennies * .01;
+	cout << "You have a total of $" << total << endl;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 }
