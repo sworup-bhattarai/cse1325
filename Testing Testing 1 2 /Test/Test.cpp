1720a8738b7646f90a606204e1cc4f0da0a5e7a2
Sworup Bhattarai <sworup.bhattarai@mavs.uta.edu>
2021-01-22 01:01:59 -0600

    01-22 01:01

--- Testing Testing 1 2 /Test/Test.cpp
+++ Testing Testing 1 2 /Test/Test.cpp
@@ -1,15 +1,84 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
+int checkTypeMenu();
+
+int openMenu(){
+	int pick;
+	
+	cout << "Please type the number corresponding with what you would like to do: \n\n" << "1) Deposit \n2) Donate All My Money \n3) Check Balence \n4) EXIT \n" << endl;
+	cout << "----------------------------------------\n" << endl;
+	pick = checkTypeMenu();
+	return pick;
+	
+	return 0;
+}
 
 
-//Inputs and checks if the value entered is an int or not and returns it to be calculated 
-int checkType(string currency) {
+int checkTypeMenu() {
 	double quantity;
 	int qcheck;
 	
 	while (true) {
-		cout << "Please enter the number of "<< currency << " you have: " ;
+		
+		
+		//checks to see if what was entered is a number or not
+		if(cin >> quantity) 
+		{
+			qcheck = quantity;
+			//checks to makesure the number is not negative 
+			if (quantity < 0 || quantity > 4)
+			{
+				cout << "Enter a number from 1-3\n" << endl;
+				cin.clear(); // clear the fail state
+				cin.ignore(99999,'\n'); // clear the buffer
+			}
+			else if (qcheck != quantity)
+			{
+				cout << "Enter Please enter whole numbers\n" << endl;
+				cin.clear(); // clear the fail state
+				cin.ignore(99999,'\n'); // clear the buffer
+			}
+			else
+			{
+			return (int) quantity;
+			}
+		}
+		
+		//no idea what this even is 
+		else if (cin.eof() || cin.bad()) // fatal states, e.g., control-d
+		{
+			cerr << "BAD DON'T DO THAT!!!!" << "\nInput terminated. Aborted." << endl;
+			exit(-1);
+		}
+		else if (cin.fail())  //catches chars 
+		{
+			cin.clear(); // clear the fail state
+			cin.ignore(99999,'\n'); // clear the buffer
+			cout << "Enter number not letters" << endl;
+
+		}
+		else 
+		{
+			return quantity;
+		}
+
+
+	}	
+
+	
+	
+}
+
+
+//Inputs and checks if the value entered is an int or not and returns it to be calculated 
+int checkTypeCash(string currency) {
+	double quantity;
+	int qcheck;
+	
+	while (true) {
+		cout << "Please enter the number of "<< currency << " you have : " ;
 		
 		//checks to see if what was entered is a number or not
 		if(cin >> quantity) 
@@ -19,6 +88,10 @@ int checkType(string currency) {
 			if (quantity < 0 )
 			{
 				cout << "Enter a number grater than 0\n" << endl;
+			}
+			else if (quantity > 100 )
+			{
+				cout << "Please enter a more believable quantity" << endl;
 			}
 			else if (qcheck != quantity)
 			{
@@ -55,7 +128,7 @@ int checkType(string currency) {
 	
 }
 
-void total () {
+double total () {
 	
 	/*double total = 0;
 	int dollars = 0;
@@ -69,35 +142,36 @@ void total () {
 	int pennies = 0;*/
 	
 	//gets input for the ammount of bills/coins and checks to see if input type is correct then adds the ammount to  the total
-	int hundreds = checkType("$100 bills");
+	int hundreds = checkTypeCash("$100 bills");
 	double total = hundreds * 100;
 	
-	int twenties = checkType("$20 bills");
+	int twenties = checkTypeCash("$20 bills");
 	total += twenties * 20;
 	
-	int tens = checkType("$10 bills ");
+	int tens = checkTypeCash("$10 bills ");
 	total += tens * 10;
 	
-	int fives = checkType("$5 bills");
+	int fives = checkTypeCash("$5 bills");
 	total += fives * 5;
 	
-	int dollars = checkType("$1 bills");
+	int dollars = checkTypeCash("$1 bills");
 	total += dollars * 1;
 	
-	int quarters = checkType("quarters");
+	int quarters = checkTypeCash("quarters");
 	total += quarters * .25;
 	
-	int dimes = checkType("dimes");
+	int dimes = checkTypeCash("dimes");
 	total += dimes * .10;
 	
-	int nickles = checkType("nickles");
+	int nickles = checkTypeCash("nickles");
 	total += nickles * .05;
 	
-	int pennies = checkType("pennies");
+	int pennies = checkTypeCash("pennies");
 	total += pennies * .01;
 	
 	
-	cout << "You have a total of $" << total << endl;	
+	cout << "You are depositing: $" << total << "\n" <<endl;
+	return total;
 }
 
 
@@ -105,7 +179,32 @@ void total () {
 
 int main(){
 	
-	total();
+	double totalMoney;
+	int menuChoice;
+	
+	cout << "HELLO AND WELCOMe TO THE BUGGY BANK" << endl;
+	cout << "Where we can guarentee that you will loose your money to some kind of bug or another!\n" << endl;
+	
+	while (true){
+		
+		menuChoice = openMenu();
+		if (menuChoice == 1){
+			totalMoney += total();
+			cout << "You have a total of $" << std::setprecision(2) << std::fixed << totalMoney << "\n\n"<< endl;
+		}
+		else if (menuChoice == 3){
+			totalMoney = 0;
+			cout << "Thank you for Donating all of your money! \nYou have $" << std::setprecision(2) << std::fixed << totalMoney << " left." << endl;
+		}
+		else if (menuChoice == 2){
+			cout << "You have $" << std::setprecision(2) << std::fixed << totalMoney << " left." << endl;
+		}
+		else if (menuChoice == 4){
+			break;
+		}
+	}
+	
+	
 	return 0;
 	
 }
