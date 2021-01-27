9de82548d1d1e44d1991dc58cc9551fadefd458e
Sworup Bhattarai <sworup.bhattarai@mavs.uta.edu>
2021-01-27 13:51:01 -0600

    1-26 13:50

--- P02/bonus/main.cpp
+++ P02/bonus/main.cpp
@@ -5,10 +5,7 @@ int main(){
 	Pet two{"Blinky", 23 , Type::BAT, 30};
 	Pet three{"Bluie", 80 , Type::WHALE, 200};
 	Pet four{"Chester", 15 , Type::KOOKABURRA , 15};
-	one.human_years();
-	two.human_years();
-	three.human_years();
-	four.human_years();
+	
 	std::cout << one.to_string() << std::endl;
 	std::cout << two.to_string() << std::endl;
 	std::cout << three.to_string() << std::endl;
--- P02/bonus/pet.cpp
+++ P02/bonus/pet.cpp
@@ -14,12 +14,14 @@ Pet::Pet( std::string n , double a , Type p, int l) : _name{n} , _age{a} , pet{p
 
 std::string Pet::to_string() 
 {
-	return _name + " is a " + ::to_string(pet) + " age " + std::to_string(_age) + " or " + std::to_string(_human_equivalent_years) + " in human-equivalent years";
+	human_years();
+	return _name + " is a " + ::to_string(pet) + " age " + std::to_string(_age) + " or " + std::to_string(_human_equivalent_years) + " in human-equivalent years.\n";
 	
 }
 void Pet::human_years()
 {
 
 	_human_equivalent_years  = (_age * 80) / pet_lifespan;
+	 
 	
 }
