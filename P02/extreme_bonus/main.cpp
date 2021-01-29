900dfaeda9fce1593ccc5643f6667a1bfbe3d4e2
Sworup Bhattarai <sworup.bhattarai@mavs.uta.edu>
2021-01-29 14:47:15 -0600

    1-29 14:47 Added to X-bonus

--- P02/extreme_bonus/main.cpp
+++ P02/extreme_bonus/main.cpp
@@ -1,22 +1,55 @@
 #include "pet.h"
 #include <vector>
+#include <iostream>
+#include <string>
 #include <bits/stdc++.h>
 
 int main(){
 	
 	std::vector<int> v;
-	int o, t, h, f;
-	Pet one{"Charlie" , 12 , Type::IGUANA};
-	Pet two{"Blinky" , 30 , Type::BAT};
-	Pet three{"Bluie" , 150 , Type::WHALE};
-	Pet four{"Chester" , 5 , Type::KOOKABURRA };
-	o = one.human_years();  
+	int o, t, h, f ;
+	double age;
+	std::string name;
+	
+	
+	
+	std::cout << ("Please enter the name of your Iguana: ");
+	std::getline(std::cin , name);
+	std::cout << ("Please enter the age of your Iguana: ");
+	std::cin >> age;
+	Pet one{name , age , Type::IGUANA};
+	std::cin.ignore(99999,'\n'); // clear the buffer
+	
+	std::cout << ("Please enter the name of your Bat: ");
+	std::getline(std::cin , name); 
+	std::cout << ("Please enter the age of your Bat: ");
+	std::cin >> age;
+	Pet two{name , age , Type::BAT};
+	std::cin.ignore(99999,'\n'); // clear the buffer
+	
+	std::cout << ("Please enter the name of your Whale: ");
+	std::getline(std::cin , name);
+	std::cout << ("Please enter the age of your Whale: ");
+	std::cin >> age;
+	Pet three{name , age , Type::WHALE};
+	std::cin.ignore(99999,'\n'); // clear the buffer
+	
+	std::cout << ("Please enter the name of your Kookaburra: ");
+	std::getline(std::cin , name);	
+	std::cout << ("Please enter the age of your Kookaburra: ");
+	std::cin >> age;
+	Pet four{name , age , Type::KOOKABURRA };
+	std::cin.ignore(99999,'\n'); // clear the buffer
+	
+	
+	
+	o = one.human_years() + 1;     //literly the WORST way of doing this  
 	v.push_back(o);  		//adds age to array
-	t = two.human_years();
+	t = two.human_years() + 2;
 	v.push_back(t);
-	h = three.human_years();
+	h = three.human_years() + 3;
 	v.push_back(h);
-	f = four.human_years();
+	f = four.human_years() + 4;
 	v.push_back(f);
 	std::sort(v.begin(), v.end()); //sorts array
 	for (int i = 0; i < v.size(); i++){
